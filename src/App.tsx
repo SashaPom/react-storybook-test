25ef2efe30497f2e6edf7142cbd8dd10a07f7374
sashapom <pomazansanya2004@gmail.com>
2026-01-09 05:39:06 +0200

    chore: add base app entry and global styles

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>React Component Library</h1>
+      <p>
+        Run Storybook: <code>npm run storybook</code>
+      </p>
+    </div>
+  );
+}
